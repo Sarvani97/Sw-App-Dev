93690c9e994d463f1fdf5edef83d97d1126c5318
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-28 19:14:05 +0530

    changes

--- project1/import.py
+++ project1/import.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import os, csv, sys;
-=======
-import os
-import csv
-import sys
-
-
->>>>>>> c0e12bf948f38e0c7dc173ad9853596c20e40672
 from flask import Flask
 from models import *
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 # Tell Flask what SQLAlchemy databas to use.
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,18 +16,10 @@ db.init_app(app)
 # with app.app_context():
 #     db.create_all()
 
-=======
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
-
->>>>>>> c0e12bf948f38e0c7dc173ad9853596c20e40672
 def main():
     with app.app_context():
         db.create_all()
         f = open("books.csv")
-<<<<<<< HEAD
         reader = csv.reader(f)
         for isbn, title, author, year in reader:
             print(isbn,title,author,year)
@@ -48,16 +31,3 @@ def main():
 if __name__ == "__main__":
     main()
     # Allows for command line interaction with Flask application
-=======
-        read = csv.reader(f)
-
-        for isbn, bname, author, year in read:
-            binfo = Book(isbn=isbn, bname=bname, author=author, year=year)
-            print(isbn,bname,author,year)
-            db.session.add(binfo)
-            db.session.commit()
-        print("Successfully added", file=sys.stdout)
-
-if __name__ == "__main__":
-    main()
->>>>>>> c0e12bf948f38e0c7dc173ad9853596c20e40672
